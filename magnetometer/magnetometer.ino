0b65bfe75a1689988d2c8feea218102161626b9a
Lukas Lazarek <lukasalazarek@gmail.com>
2016-01-24 13:27:07 -0500

    Cleaned up comments.

--- magnetometer/magnetometer.ino
+++ magnetometer/magnetometer.ino
@@ -1,33 +1,10 @@
-/***************************************************************************
-  This is a library example for the HMC5883 magnentometer/compass
+/* HMC5883 magnentometer heading code
+   Adapted by Lukas Lazarek from example code in Adafruit_HMC5883_U library written by Kevin Townsend.
 
-  Designed specifically to work with the Adafruit HMC5883 Breakout
-  http://www.adafruit.com/products/1746
- 
-  *** You will also need to install the Adafruit_Sensor library! ***
-
-  These displays use I2C to communicate, 2 pins are required to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Kevin Townsend for Adafruit Industries with some heading example from
-  Love Electronics (loveelectronics.co.uk)
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the version 3 GNU General Public License as
- published by the Free Software Foundation.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- ***************************************************************************/
+   Libraries required: Adafruit_Sensor, Adafruit_HMC5883_U; obtained from
+   https://learn.adafruit.com/adafruit-hmc5883l-breakout-triple-axis-magnetometer-compass-sensor/wiring-and-test
+   along with wiring instructions.
+ */
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -55,17 +32,15 @@ void displaySensorDetails(void)
 void setup(void) 
 {
   Serial.begin(9600);
-  Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
   
   /* Initialise the sensor */
   if(!mag.begin())
   {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    Serial.println("Error initializing HMC5883");
     while(1);
   }
   
-  /* Display some basic information on this sensor */
+  /* Display information about the HMC5883 */
   displaySensorDetails();
 }
 
@@ -80,15 +55,11 @@ void loop(void)
   Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
   Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");*/
 
-  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  // Sensor is held sideways (with Y being the vertical axis), so heading is calculated using X and Z
   float heading = atan2(event.magnetic.z, event.magnetic.x);
   
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.244346;
+  // Need to account of error based upon location, find declination angle for location at http://www.magnetic-declination.com/
+  float declinationAngle = 0.244346; // radians
   heading += declinationAngle;
   
   // Correct for when signs are reversed.
@@ -101,9 +72,8 @@ void loop(void)
    
   // Convert radians to degrees for readability.
   float headingDegrees = heading * 180/M_PI; 
-  
-  //Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
 
+  // Print heading in terms of N/S/E/W
   if(headingDegrees >= 338 || headingDegrees < 4) Serial.println("SE");
   else if(headingDegrees >= 4 && headingDegrees < 31.75) Serial.println("E");
   else if(headingDegrees >= 31.75 && headingDegrees < 61.25) Serial.println("NE");
